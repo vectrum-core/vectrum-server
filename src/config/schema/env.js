5a719fa4ecca63b3777ea455574483b10e10b941
Aleksandr Ivannikov <a.n.ivannikov@yandex.ru>
2020-11-30 02:04:41 +0700

    Create env.js

---
+++ src/config/schema/env.js
@@ -0,0 +1,11 @@
+const schema = {
+  env: {
+    doc: 'The application environment.',
+    format: ['production', 'development', 'test'],
+    default: 'development',
+    env: 'NODE_ENV',
+    arg: 'node-env',
+  },
+};
+
+module.exports = schema;
