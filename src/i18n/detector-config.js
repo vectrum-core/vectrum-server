41aecdbadcd3b5f8b0c639af93dcc83bc9330a56
Aleksandr Ivannikov <a.n.ivannikov@yandex.ru>
2020-11-30 05:30:13 +0700

    Update detector-config.js

--- src/i18n/detector-config.js
+++ src/i18n/detector-config.js
@@ -1,21 +1,27 @@
+const cfg = require('../config');
+
+
+
+const inProduction = cfg.get('env') === 'production';
+
 module.exports = {
   // order and from where user language should be detected
   // [ 'path', 'session', 'querystring', 'cookie', 'header' ],
-  order: ['path', 'session', 'querystring', 'cookie'],
+  order: ['path'],
 
   // keys or params to lookup language from
-  lookupQuerystring: 'lng',
+  //lookupQuerystring: 'lng',
   lookupCookie: 'lng',
   //lookupHeader: 'accept-language',
-  lookupSession: 'lng',
+  //lookupSession: 'lng',
   //lookupPath: 'lng',
   lookupFromPathIndex: 0,
 
   // cache user language
-  caches: ['cookie'], //false, // ['cookie']
+  caches: ['cookie'],
 
   // optional expire and domain for set cookie
   //cookieExpirationDate: new Date(),
   //cookieDomain: 'myDomain',
-  //cookieSecure: true, // if need secure cookie
+  cookieSecure: inProduction ? true : false,
 };
