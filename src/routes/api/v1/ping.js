874a79f88548e13ae9fb14ae3c2936913eafe1aa
Aleksandr Ivannikov <a.n.ivannikov@yandex.ru>
2020-11-30 04:12:15 +0700

    src/routes/api/v1

---
+++ src/routes/api/v1/ping.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const router = express.Router();
+const SS = require('../../../lib/smart-stringify');
+const log = require('../../../logger').getLogger('router->api->v1->ping');
+
+
+
+router.get('/',
+  async (req, res, next) => {
+    const logTrace = 'GET';
+    const answer = { jsonrpc: '2.0', id: null, ok: false, error: undefined, result: undefined, };
+
+    answer.result = 'pong';
+    answer.ok = true;
+
+    log.trace(logTrace, SS(answer));
+    return res.json(answer);
+  }
+);
+
+router.post('/',
+  async (req, res, next) => {
+    const logTrace = 'POST';
+    const answer = { jsonrpc: '2.0', id: null, ok: false, error: undefined, result: undefined, };
+
+    answer.result = 'pong';
+    answer.ok = true;
+
+    log.trace(logTrace, SS(answer));
+    return res.json(answer);
+  }
+);
+
+
+module.exports = router;
---
+++ src/routes/api/v1/v1.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const pingRouter = require('./ping');
+const userRouter = require('./user');
+
+
+
+router.use('/ping', pingRouter);
+router.use('/user', userRouter);
+
+module.exports = router;
