175521c64dc4dd3fb9fbab49178fb4c049287338
Aleksandr Ivannikov <a.n.ivannikov@yandex.ru>
2020-11-30 02:08:03 +0700

    Create session-store.js

---
+++ src/config/schema/session-store.js
@@ -0,0 +1,43 @@
+const isProduction = process.env.NODE_ENV === 'production';
+
+
+
+const schema = {
+  secret: {
+    doc: 'session store secret',
+    format: String,
+    default: isProduction ? undefined : 'keyboard^cat^vectrum',
+  },
+  algorithm: {
+    doc: 'session store algorithm (Allows for changes to the default symmetric encryption cipher; default is GCM. See crypto.getCiphers() for supported algorithms.)',
+    format: String,
+    default: 'GCM',
+  },
+  hashing: {
+    doc: 'session store hashing (May be used to change the default hashing algorithm; default is sha512. See crypto.getHashes() for supported hashing algorithms.)',
+    format: String,
+    default: 'sha512',
+  },
+  encodeas: {
+    doc: 'session store encodeas (Specify to change the session data cipher text encoding.Default is hex.)',
+    format: String,
+    default: 'hex',
+  },
+  key_size: {
+    doc: 'session store key_size (When using varying algorithms the key size may be used.Default is 32 based on the AES blocksize.)',
+    format: Number,
+    default: 32,
+  },
+  iv_size: {
+    doc: 'session store iv_size (This can be used to adjust the default IV size if a different algorithm requires a different size.Default is 16.)',
+    format: Number,
+    default: 16,
+  },
+  at_size: {
+    doc: 'session store at_size (When using newer AES modes such as the default GCM or CCM an authentication tag size can be defined; default is 16.)',
+    format: Number,
+    default: 16,
+  },
+};
+
+module.exports = schema;
