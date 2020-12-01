ed909b67e8d11dfec7c12f712a0f3aa26427655a
Aleksandr Ivannikov <a.n.ivannikov@yandex.ru>
2020-12-01 13:28:00 +0700

    Create emails.js

---
+++ src/mongoose/models/emails.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const AutoIncrementPlugin = require('mongoose-sequence')(mongoose);
+const emailValidator = require('./lib/validator-email');
+
+
+
+const name = 'emails';
+
+const schema = new Schema({
+  num: { type: Number, min: 1, },
+
+  _id: {
+    type: String, lowercase: true, trim: true,
+    required: true, unique: true,
+    validate: emailValidator,
+  },
+
+  confirmed: { type: Boolean, index: true, default: false, },
+
+}, {
+  timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' },
+});
+
+
+schema.plugin(
+  AutoIncrementPlugin,
+  { id: name, inc_field: 'num', start_seq: 1, },
+);
+
+
+module.exports = mongoose.model(name, schema);
+module.exports.name = name;
+module.exports.schema = schema;
