362bf954ac80915dd45ba5e17acd9fb8b2fa60b7
Aleksandr Ivannikov <a.n.ivannikov@yandex.ru>
2020-12-01 14:00:24 +0700

    Update index.js

--- src/mongoose/models/index.js
+++ src/mongoose/models/index.js
@@ -1,5 +1,6 @@
 module.exports = {
   continents: require('./continents'),
+  countries: require('./countries'),
   emails: require('./emails'),
   passwords: require('./passwords'),
   users: require('./users'),
