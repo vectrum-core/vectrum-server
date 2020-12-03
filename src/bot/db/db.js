90bc6feed9c92ddd39867bead8cbb72b0a39c700
Aleksandr Ivannikov <a.n.ivannikov@yandex.ru>
2020-12-03 09:25:45 +0700

    Create db.js

---
+++ src/bot/db/db.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const User = mongoose.model('users');
+const TGUser = mongoose.model('tg_users');
+const Language = mongoose.model('Language');
+const logger = require('../../logger');
+
+
+
+const log = logger.getLogger('BOT:DB');
+
+
+module.exports = {
+};
