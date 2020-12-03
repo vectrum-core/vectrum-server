d5e51cac4300dd342a9f1360ff34ca6e0bf5e66d
Aleksandr Ivannikov <a.n.ivannikov@yandex.ru>
2020-12-03 18:52:00 +0700

    Create i18n-prepare.js

---
+++ src/mailer/i18n-prepare.js
@@ -0,0 +1,17 @@
+module.exports = (options = {}) => {
+  const { i18n } = options;
+
+  return async (mail, done) => {
+    if (!mail || mail.data || mail.data.context || mail.data.context.language) {
+      return done();
+    }
+
+    if (mail.data.context.language && mail.data.context.language !== i18n.language)
+      await i18n.changeLanguage(mail.data.context.language);
+
+    if (mail.data.subject)
+      mail.data.subject = i18n.t(mail.data.subject);
+
+    return done();
+  }
+}
