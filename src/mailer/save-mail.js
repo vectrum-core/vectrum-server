40a19396024b317027660952292897588108c17a
Aleksandr Ivannikov <a.n.ivannikov@yandex.ru>
2020-12-03 18:49:00 +0700

    Create save-mail.js

---
+++ src/mailer/save-mail.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const Mail = mongoose.model('mails');
+const log = require('../logger').getLogger('MAILER:compile:saveMail');
+
+
+
+module.exports = async (mail, done) => {
+  if (!mail || !mail.data)
+    return done();
+
+  if (mail.data.notSave)
+    return done();
+
+  const doc = new Mails(mail.data);
+  await doc.save();
+  mail.data = doc.toObject();
+
+  return done();
+};
+
