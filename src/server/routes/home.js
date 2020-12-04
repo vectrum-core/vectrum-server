cdf2dd41cac5f2b778eb6e8360a3092c5d5b4c53
Aleksandr Ivannikov <a.n.ivannikov@yandex.ru>
2020-12-04 13:13:24 +0700

    Update home.js

--- src/server/routes/home.js
+++ src/server/routes/home.js
@@ -7,10 +7,8 @@ router.get('/', (req, res, next) => {
   res.render('index', {
     header: `${req.t('Project Title')}`,
     subHeader: `${req.t('Project Slogan')}`,
-    lang: req.language,
     title: `${req.t('Project Title')}`,
     description: `${req.t('Project Slogan')}`,
-    noscript: req.t('noscript'),
   });
 });
 
