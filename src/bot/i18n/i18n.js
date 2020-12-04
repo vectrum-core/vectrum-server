54536370c0bf603e982f97755eee36adb27e1726
Aleksandr Ivannikov <a.n.ivannikov@yandex.ru>
2020-12-04 08:46:41 +0700

    Update i18n.js

--- src/bot/i18n/i18n.js
+++ src/bot/i18n/i18n.js
@@ -1,11 +1,12 @@
 const path = require('path');
+const cfg = require('../../config');
 const I18n = require('../telegraf-i18n');
 
 
 
 const i18n = new I18n({
   directory: path.resolve(__dirname, '../locales'),
-  defaultLanguage: 'ru',
+  defaultLanguage: cfg.get('language'),
   allowMissing: true,
   defaultLanguageOnMissing: true,
   sessionName: 'session',
