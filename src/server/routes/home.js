75f4561ed37aa110e799bcbb311a4dc748f94832
Aleksandr Ivannikov <a.n.ivannikov@yandex.ru>
2020-12-16 12:03:44 +0300

    Update home.js

--- src/server/routes/home.js
+++ src/server/routes/home.js
@@ -6,9 +6,9 @@ const router = express.Router();
 router.get('/', (req, res, next) => {
   res.render('index', {
     header: `${req.t('Project Title')}`,
-    subHeader: `${req.t('Project Slogan')}`,
+    subHeader: `${req.t('Project Title')}`,
     title: `${req.t('Project Title')}`,
-    description: `${req.t('Project Slogan')}`,
+    description: `${req.t('Project Title')}`,
   });
 });
 
