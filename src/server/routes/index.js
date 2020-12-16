59e50dae12559a9963a6023a707679a663247f00
Aleksandr Ivannikov <a.n.ivannikov@yandex.ru>
2020-12-16 12:04:55 +0300

    Update index.js

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 const cors = require('cors');
 const homeRouter = require('./home');
+const explorerRouter = require('./explorer');
+const profileRouter = require('./profile');
+const walletRouter = require('./wallet');
 const authRouter = require('./auth');
 const apiRouter = require('./api');
 
@@ -9,18 +12,19 @@ const apiRouter = require('./api');
 
 router.use('/', homeRouter);
 router.use('/index.html', homeRouter);
-router.use('/wallet', homeRouter);
+
+router.use('/explorer', explorerRouter);
+router.use('/profile', profileRouter);
+router.use('/wallet', walletRouter);
 router.use('/exchange', homeRouter);
 router.use('/marketplace', homeRouter);
 
 
 router.use('/auth', authRouter);
 
-
 router.use('/api',
   cors(),
   apiRouter
 );
-
 
 module.exports = router;
